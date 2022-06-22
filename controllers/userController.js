3bc18ac9d81693ac89d64bd88afbb7697687d7d1
lukaofirst <lucasmacedo123@gmail.com>
2022-06-22 20:46:47 -0300

    adding register and login user methods without encrypt password

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,11 +15,40 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('User already exists');
     }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid credentials');
+    }
 });
 
 module.exports = { registerUser, loginUser };
